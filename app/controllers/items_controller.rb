b89ce3505b842a8c9aa4ebe371d01a6b0f191802
ksdueki <ueki@aok-net.com>
2020-05-06 20:46:25 +0900

    商品詳細の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: %i[show]
+
   def index
     @items = Item.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
