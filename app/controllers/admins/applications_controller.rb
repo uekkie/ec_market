3a9e939d9d1b85892c9ccf430b680b230a5476e2
hirocueki <hirocueki@gmail.com>
2020-05-16 16:40:30 +0900

    admin_signed_in? をつかうようにした

--- app/controllers/admins/applications_controller.rb
+++ app/controllers/admins/applications_controller.rb
@@ -1,10 +1,3 @@
 class Admins::ApplicationsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :is_not_admin
-
-  private
-
-  def is_not_admin
-    redirect_to root_path, alert: "管理者のみアクセス可能です" unless current_user.admin?
-  end
+  before_action :admin_signed_in?
 end
