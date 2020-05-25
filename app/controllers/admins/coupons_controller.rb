efae2df36296f35bf07fd69f13b02980d48f3b9d
hirocueki <hirocueki@gmail.com>
2020-05-26 07:50:01 +0900

    管理者はクーポンの参照、編集、削除ができる

--- app/controllers/admins/coupons_controller.rb
+++ app/controllers/admins/coupons_controller.rb
@@ -1,11 +1,19 @@
 class Admins::CouponsController < ApplicationController
+  before_action :set_coupon, only: %i[show edit update destroy]
 
   def index
     @coupons = Coupon.recent
   end
 
+  def show
+
+  end
+
   def new
     @coupon = Coupon.new
+  end
+
+  def edit
   end
 
 
@@ -19,9 +27,27 @@ class Admins::CouponsController < ApplicationController
     end
   end
 
+
+  def update
+    if @coupon.update(coupon_params)
+      redirect_to admins_coupons_url, notice: '更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @coupon.destroy!
+    redirect_to admins_coupons_url, notice: '削除しました'
+  end
+
   private
 
   def coupon_params
     params.require(:coupon).permit(:point)
+  end
+
+  def set_coupon
+    @coupon = Coupon.find(params[:id])
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -21,8 +21,47 @@ RSpec.describe "Users", type: :system do
     let!(:admin) { create(:admin) }
     let!(:user) { create(:user) }
 
+    before { sign_in admin }
+
+    it 'クーポンのCRUDができる' do
+      visit admins_coupons_path
+      click_on 'クーポンコードを発行'
+
+      fill_in 'ポイント', with: 1000
+      expect {
+        click_on '登録する'
+      }.to change { Coupon.count }.by(1)
+
+      coupon = Coupon.last
+      expect(coupon.point).to eq 1000
+
+      click_on coupon.display_code
+
+      expect(page).to have_content '1000'
+      expect(page).to have_content coupon.display_code
+
+      first('tbody tr').click_link '変更'
+
+      fill_in 'ポイント', with: 2000
+      click_on '更新する'
+
+      coupon.reload
+      expect(coupon.point).to eq 2000
+    end
+
+    it 'クーポンを削除できる', js: true do
+      create(:coupon)
+
+      visit admins_coupons_path
+      expect {
+        first('tbody tr').click_link '削除'
+        expect(page.driver.browser.switch_to.alert.text).to eq '削除してよろしいですか？'
+        page.accept_confirm
+        expect(current_path).to eq admins_coupons_path
+      }.to change { Coupon.count }.by(-1)
+    end
+
     it 'ユーザーのポイントを増減できる' do
-      sign_in admin
 
       visit edit_users_point_path(user)
 
