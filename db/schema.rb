d785ca6fe81c73be2fc08b3ad953934a4102920a
hirocueki <hirocueki@gmail.com>
2020-05-30 11:23:05 +0900

    注文時にポイントを利用できる

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_091327) do
+ActiveRecord::Schema.define(version: 2020_05_30_004538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_091327) do
     t.integer "tax", default: 8, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_point", default: 0, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
