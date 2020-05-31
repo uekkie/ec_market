132606f6f72cb68ccc40015c8ee762dc1736233b
hirocueki <hirocueki@gmail.com>
2020-05-31 23:47:25 +0900

    商品に業者idを持たせるように（null許可）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_122223) do
+ActiveRecord::Schema.define(version: 2020_05_31_141300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2020_05_31_122223) do
     t.integer "position", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -143,6 +145,7 @@ ActiveRecord::Schema.define(version: 2020_05_31_122223) do
   add_foreign_key "coupons", "users"
   add_foreign_key "goods", "posts"
   add_foreign_key "goods", "users"
+  add_foreign_key "items", "merchants"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
